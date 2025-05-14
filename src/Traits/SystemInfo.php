86098d9b4f50cf58be08e678c8174e6e453b3148
Jacco Amersfoort <jacco.amersfoort@monta.nl>
2025-05-14 15:11:08 +0200

    #19109 Declare body properties in SystemInfo trait
    centralize property names for request call to

--- src/Traits/SystemInfo.php
+++ src/Traits/SystemInfo.php
@@ -10,6 +10,21 @@ trait SystemInfo
     /** @var string - Identifies the system information in the body */
     public const SYSTEM_INFO_NAME = 'systemInformation';
 
+    /* Constants to determine the system info body. Reference these from client modules. */
+    public const CORE_SOFTWARE = 'coreSoftware';
+
+    public const CORE_VERSION = 'coreVersion';
+
+    public const CHECKOUT_API_WRAPPER_VERSION = 'checkoutApiWrapperVersion';
+
+    public const MODULE_NAME = 'moduleName';
+
+    public const MODULE_VERSION = 'moduleVersion';
+
+    public const PHP_VERSION = 'phpVersion';
+
+    public const OPERATING_SYSTEM = 'operatingSystem';
+
     /** @var array */
     protected array $systemInfo = [];
 
