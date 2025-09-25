c5379910acd5c69d256b2d7c97276f9c2c8029b4
Jacco Amersfoort <jacco.amersfoort@monta.nl>
2025-09-25 11:18:22 +0200

    #90837 Clean up Session class
    Convert path in separate function and clean up path

--- src/Objects/Session.php
+++ src/Objects/Session.php
@@ -7,26 +7,42 @@ namespace Monta\CheckoutApiWrapper\Objects;
 
 class Session
 {
-    /** @var string  */
+    /** @var string - Cache prefix to distinguish values in session */
     protected const SESSION_PREFIX = 'monta_session_data_';
 
     /**
      * @param string $path
-     * @param $value
+     * @param $value - Array, string, anything that can be saved in session
      * @return void
      */
-    public static function save(string $path, $value)
+    public static function save(string $path, $value): void
     {
-        $_SESSION[self::SESSION_PREFIX . $path] = $value;
+        $_SESSION[self::sessionPath($path)] = $value;
     }
 
     /**
      * @param string $path
-     * @return mixed
+     * @return mixed - Could be anything
      */
-    public static function get(string $path)
+    public static function get(string $path): mixed
     {
-        return $_SESSION[self::SESSION_PREFIX . $path] ?? null;
+        return $_SESSION[self::sessionPath($path)] ?? null;
+    }
+
+    /** Convert user path into prefixed, usable path
+     *
+     * @param string $path
+     * @return string
+     */
+    protected static function sessionPath(string $path): string
+    {
+        // Add prefix to path
+        return self::SESSION_PREFIX
+            // convert special characters
+            . htmlentities(
+            // make lowercase (no functional difference between case in address values
+                strtolower($path)
+            );
     }
 
     /** Flush session cache
