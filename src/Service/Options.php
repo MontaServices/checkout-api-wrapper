6376a21f795bdde395627fb330dbd8a09f7ba414
Jacco Amersfoort <jacco.amersfoort@monta.nl>
2025-11-13 13:02:46 +0100

    #25375 Simplify combining arrays
    Replace foreach with simple array function

--- src/Service/Options.php
+++ src/Service/Options.php
@@ -18,13 +18,11 @@ class Options
     {
         // Convert JSON into array, splat into property constructor
         $converted = json_decode($json, true);
-        $props = [];
-        // Copy each field into property
-        foreach ($converted as $key => $value) {
-            if (property_exists(Option::class, $key)) {
-                $props[$key] = $value;
-            }
-        }
+        // Get array values with only the keys that are a property
+        $props = array_intersect_key(
+            $converted,
+            get_class_vars(Option::class)
+        );
         // TODO also include shipper options in calculations
         return !empty($props) ? new Option(...$props) : null;
     }
