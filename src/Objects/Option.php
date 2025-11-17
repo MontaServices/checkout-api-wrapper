1312b64d9c4b1d1da38732bcf40862e79124c907
Jacco Amersfoort <jacco.amersfoort@monta.nl>
2025-11-17 16:22:51 +0100

    #25375 Determine Type by Type for Option
    Pickup or Delivery type must be determined by available data (instead of passing along from frontend)

--- src/Objects/Option.php
+++ src/Objects/Option.php
@@ -109,14 +109,14 @@ class Option
     }
 
     /** Convert selected Option to JSON in proper structure.
-     * Output format determined by old Montapacking module for backwards compatibility
+     * Output format determined by old Montapacking module output for backwards compatibility
      *
      * @return string - JSON string with all it's data ready for DB writing or API output
      */
     public function toJson(): string
     {
         return json_encode([
-            'type' => 'delivery', // TODO determine type somehow
+            'type' => $this->getShippingType(),
             'details' => [
                 'short_code' => $this->getAdditionalData('shipper'),
                 'options' => [], // TODO shipper options
@@ -124,12 +124,26 @@ class Option
             'additional_info' => [
                 [
                     'code' => $this->getCode(),
-                    'name' => $this->getAdditionalData('shipper'),
+                    'name' => $this->getAdditionalData('displayName'),
                     'date' => date("Y-m-d H:i:s"), // TODO get desired delivery datetime
                     'time' => date("H:i - H:i"), // TODO desired delivery time slot
                     'price' => $this->getPrice(), // TODO shipper price only
                     'total_price' => $this->getPrice(), // TODO shipper price + shipper options
                 ]
             ]]);
+    }
+
+    /** Determine shipping type
+     * @return string
+     */
+    protected function getShippingType(): string
+    {
+        $type = 'delivery';
+
+        // Pickup point has no delivery type but has a postal code
+        if (!$this->getAdditionalData('deliveryType') && $this->getAdditionalData('postalCode')) {
+            $type = 'pickup';
+        }
+        return $type;
     }
 }
